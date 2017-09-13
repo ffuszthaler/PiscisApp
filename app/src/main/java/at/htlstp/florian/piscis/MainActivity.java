93bab409a923a853132703d2fc460bbcc8109cd0
Florian Fußthaler <flo.fuszthaler@gmail.com>
2017-09-13 13:32:15 +0200

    Initial commit

---
+++ app/src/main/java/at/htlstp/florian/piscis/MainActivity.java
@@ -0,0 +1,36 @@
+package at.htlstp.florian.piscis;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+public class MainActivity extends Activity {
+
+    private WebView mWebView;
+
+    @Override
+    public void onBackPressed() {
+        if(mWebView.canGoBack()) {
+            mWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mWebView = (WebView) findViewById(R.id.activity_main_webview);
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mWebView.loadUrl("https://www.google.at");
+        mWebView.setWebViewClient(new WebViewClient());
+    }
+}
